e042004bac2aa1cc037968e8ec7e44e006427721
Michal Turek <mixalturek@users.sf.net>
2013-01-13 21:02:28 +0100

    Javadoc - Package info files created.

---
+++ src/net/sourceforge/anotherfsm/package-info.java
@@ -0,0 +1,39 @@
+/*
+ *  Copyright 2013 Michal Turek, Another FSM
+ *
+ *      http://anotherfsm.sourceforge.net/
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/**
+ * Another FSM is yet another implementation of state machines for programming 
+ * language Java.
+ * 
+ * <h3>Features</h3>
+ * 
+ * <ul>
+ * <li>Deterministic state machine, optionally with timeouts.</li>
+ * <li>Global and local listeners of transitions and state enter/exit.</li>
+ * <li>Type-based and equals-based event preprocessors.</li>
+ * <li>Predefined and user defined events.</li>
+ * <li>Separated state machine building, registration of listeners and processing of events.</li>
+ * <li>Generalized logging, whatever backend can be injected.</li>
+ * <li>Apache 2 license, version 2.0.</li>
+ * <li>Well-designed API, fully documented code, unit tests.</li>
+ * </ul>
+ *
+ * @author Michal Turek
+ */
+package net.sourceforge.anotherfsm;
+
