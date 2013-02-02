081870098e4b82931583452c8a2a6d88ac34ac0a
Michal Turek <mixalturek@users.sf.net>
2013-02-02 21:31:20 +0100

    Code generator - Parser of parameters implemented.

---
+++ src/net/sourceforge/anotherfsm/qfsm/CodeGeneratorParameters.java
@@ -0,0 +1,181 @@
+/*
+ *  Copyright 2013 Michal Turek, AnotherFSM
+ *
+ *      http://anotherfsm.sourceforge.net/
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package net.sourceforge.anotherfsm.qfsm;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Helper class to parse the program input parameters and arguments.
+ * 
+ * @author Michal Turek
+ */
+class CodeGeneratorParameters {
+	/** Show usage. */
+	private boolean usage = false;
+
+	/** Verbose output. */
+	private boolean verbose = false;
+
+	/** Force, override output files. */
+	private boolean force = false;
+
+	/** The qfsm file */
+	private String configFile;
+
+	/** The configuration file */
+	private String qfsmFile;
+
+	/**
+	 * Create the object.
+	 * 
+	 * @param args
+	 *            the parameters to parse
+	 * @throws QfsmException
+	 *             if the parameters are invalid
+	 */
+	public CodeGeneratorParameters(String[] args) throws QfsmException {
+		String[] params = preprocessParameters(args);
+		List<String> unexpected = new LinkedList<String>();
+
+		for (int i = 0; i < params.length; ++i) {
+			switch (params[i]) {
+			case "-h":
+			case "--help":
+				usage = true;
+				break;
+
+			case "-v":
+			case "--verbose":
+				verbose = true;
+				break;
+
+			case "-f":
+			case "--force":
+				force = true;
+				break;
+
+			case "-c":
+			case "--config-file":
+				if (i + 1 >= params.length) {
+					throw new QfsmException(
+							"Missing second argument of -c | --config-file");
+				}
+
+				++i;
+				configFile = params[i];
+				break;
+
+			case "-m":
+			case "--qfsm-file":
+				if (i + 1 >= params.length) {
+					throw new QfsmException(
+							"Missing second argument of -m | --qfsm-file");
+				}
+
+				++i;
+				qfsmFile = params[i];
+				break;
+
+			default:
+				unexpected.add(params[i]);
+				break;
+			}
+		}
+
+		if (configFile == null) {
+			throw new QfsmException(
+					"Missing mandatory parameter: -c | --config-file");
+		}
+
+		if (qfsmFile == null) {
+			throw new QfsmException(
+					"Missing mandatory parameter: -m | --qfsm-file");
+		}
+
+		if (!unexpected.isEmpty()) {
+			throw new QfsmException("Unexpected parameters: " + unexpected);
+		}
+	}
+
+	/**
+	 * Preprocess multi value parameters, split to parts delimited by "=".
+	 * 
+	 * @param args
+	 *            the input arguments
+	 * @return the preprocessed arguments
+	 */
+	private final String[] preprocessParameters(String[] args) {
+		List<String> out = new LinkedList<String>();
+
+		for (int i = 0; i < args.length; ++i) {
+			for (String part : args[i].split("=", 2))
+				out.add(part);
+		}
+
+		return out.toArray(new String[0]);
+	}
+
+	/**
+	 * Write usage to the standard output.
+	 */
+	public static void showUsage() {
+		System.out.println("java CodeGenerator [arguments] parameters");
+		System.out.println();
+		System.out.println("Arguments: ");
+		System.out.println("\t[-h | --help]            Show this usage.");
+		System.out.println("\t[-v | --verbose]         Verbose output.");
+		System.out.println("\t[-f | --force]           Override output files.");
+		System.out.println();
+		System.out.println("Parameters: ");
+		System.out.println("\t-c | --config-file file  Configuration file.");
+		System.out.println("\t-m | --qfsm-file file    Qfsm file.");
+	}
+
+	/**
+	 * Dump the parsed parameters.
+	 */
+	public void dumpParameters() {
+		System.out.println("Show usage: " + (usage ? "yes" : "no"));
+		System.out.println("Verbose output: " + (verbose ? "yes" : "no"));
+		System.out.println("Override output files: " + (force ? "yes" : "no"));
+		System.out.println("Configuration file: " + configFile);
+		System.out.println("Qfsm file: " + qfsmFile);
+	}
+
+	public boolean isUsage() {
+		return usage;
+	}
+
+	public boolean isVerbose() {
+		return verbose;
+	}
+
+	public boolean isForce() {
+		return force;
+	}
+
+	public String getConfigFile() {
+		return configFile;
+	}
+
+	public String getQfsmFile() {
+		return qfsmFile;
+	}
+}
---
+++ src_tests/net/sourceforge/anotherfsm/qfsm/CodeGeneratorParametersTest.java
@@ -0,0 +1,285 @@
+package net.sourceforge.anotherfsm.qfsm;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class CodeGeneratorParametersTest {
+	@Test
+	public void testDefault() {
+		String[] args = { "-m", "unused", "-c", "unused" };
+
+		try {
+			CodeGeneratorParameters params = new CodeGeneratorParameters(args);
+			assertFalse(params.isForce());
+			assertFalse(params.isUsage());
+			assertFalse(params.isVerbose());
+		} catch (QfsmException e) {
+			fail("Should not be executed: " + e);
+		}
+	}
+
+	@Test
+	public void testIsUsageShort() {
+		String[] args = { "-m", "unused", "-c", "unused", "-h" };
+
+		try {
+			CodeGeneratorParameters params = new CodeGeneratorParameters(args);
+			assertTrue(params.isUsage());
+		} catch (QfsmException e) {
+			fail("Should not be executed: " + e);
+		}
+	}
+
+	@Test
+	public void testIsUsageLong() {
+		String[] args = { "-m", "unused", "-c", "unused", "--help" };
+
+		try {
+			CodeGeneratorParameters params = new CodeGeneratorParameters(args);
+			assertTrue(params.isUsage());
+		} catch (QfsmException e) {
+			fail("Should not be executed: " + e);
+		}
+	}
+
+	@Test
+	public void testIsVerboseShort() {
+		String[] args = { "-m", "unused", "-c", "unused", "-v" };
+
+		try {
+			CodeGeneratorParameters params = new CodeGeneratorParameters(args);
+			assertTrue(params.isVerbose());
+		} catch (QfsmException e) {
+			fail("Should not be executed: " + e);
+		}
+	}
+
+	@Test
+	public void testIsVerboseLong() {
+		String[] args = { "-m", "unused", "-c", "unused", "--verbose" };
+
+		try {
+			CodeGeneratorParameters params = new CodeGeneratorParameters(args);
+			assertTrue(params.isVerbose());
+		} catch (QfsmException e) {
+			fail("Should not be executed: " + e);
+		}
+	}
+
+	@Test
+	public void testIsForceShort() {
+		String[] args = { "-m", "unused", "-c", "unused", "-f" };
+
+		try {
+			CodeGeneratorParameters params = new CodeGeneratorParameters(args);
+			assertTrue(params.isForce());
+		} catch (QfsmException e) {
+			fail("Should not be executed: " + e);
+		}
+	}
+
+	@Test
+	public void testIsForceLong() {
+		String[] args = { "-m", "unused", "-c", "unused", "--force" };
+
+		try {
+			CodeGeneratorParameters params = new CodeGeneratorParameters(args);
+			assertTrue(params.isForce());
+		} catch (QfsmException e) {
+			fail("Should not be executed: " + e);
+		}
+	}
+
+	@Test
+	public void testGetConfigFileShort() {
+		String[] args = { "-m", "unused", "-c", "test" };
+
+		try {
+			CodeGeneratorParameters params = new CodeGeneratorParameters(args);
+			assertEquals("test", params.getConfigFile());
+		} catch (QfsmException e) {
+			fail("Should not be executed: " + e);
+		}
+	}
+
+	@Test
+	public void testGetConfigFileLong() {
+		String[] args = { "-m", "unused", "--config-file", "test" };
+
+		try {
+			CodeGeneratorParameters params = new CodeGeneratorParameters(args);
+			assertEquals("test", params.getConfigFile());
+		} catch (QfsmException e) {
+			fail("Should not be executed: " + e);
+		}
+	}
+
+	@Test
+	public void testGetConfigFileShortPreprocess() {
+		String[] args = { "-m", "unused", "-c=test" };
+
+		try {
+			CodeGeneratorParameters params = new CodeGeneratorParameters(args);
+			assertEquals("test", params.getConfigFile());
+		} catch (QfsmException e) {
+			fail("Should not be executed: " + e);
+		}
+	}
+
+	@Test
+	public void testGetConfigFileLongPreprocess() {
+		String[] args = { "-m", "unused", "--config-file=test" };
+
+		try {
+			CodeGeneratorParameters params = new CodeGeneratorParameters(args);
+			assertEquals("test", params.getConfigFile());
+		} catch (QfsmException e) {
+			fail("Should not be executed: " + e);
+		}
+	}
+
+	@Test
+	public void testGetConfigFileShortBroken() {
+		String[] args = { "-m", "unused", "-c" };
+
+		try {
+			new CodeGeneratorParameters(args);
+			fail("Should not be executed");
+		} catch (QfsmException e) {
+			assertEquals("Missing second argument of -c | --config-file",
+					e.getMessage());
+		}
+	}
+
+	@Test
+	public void testGetConfigFileLongBroken() {
+		String[] args = { "-m", "unused", "--config-file" };
+
+		try {
+			new CodeGeneratorParameters(args);
+			fail("Should not be executed");
+		} catch (QfsmException e) {
+			assertEquals("Missing second argument of -c | --config-file",
+					e.getMessage());
+		}
+	}
+
+	@Test
+	public void testGetConfigFileUndefined() {
+		String[] args = { "-m", "unused" };
+
+		try {
+			new CodeGeneratorParameters(args);
+			fail("Should not be executed");
+		} catch (QfsmException e) {
+			assertEquals("Missing mandatory parameter: -c | --config-file",
+					e.getMessage());
+		}
+	}
+
+	@Test
+	public void testGetQfsmFileShort() {
+		String[] args = { "-c", "unused", "-m", "test" };
+
+		try {
+			CodeGeneratorParameters params = new CodeGeneratorParameters(args);
+			assertEquals("test", params.getQfsmFile());
+		} catch (QfsmException e) {
+			fail("Should not be executed: " + e);
+		}
+	}
+
+	@Test
+	public void testGetQfsmFileLong() {
+		String[] args = { "-c", "unused", "--qfsm-file", "test" };
+
+		try {
+			CodeGeneratorParameters params = new CodeGeneratorParameters(args);
+			assertEquals("test", params.getQfsmFile());
+		} catch (QfsmException e) {
+			fail("Should not be executed: " + e);
+		}
+	}
+
+	@Test
+	public void testGetQfsmFileShortPreprocess() {
+		String[] args = { "-c", "unused", "-m=test" };
+
+		try {
+			CodeGeneratorParameters params = new CodeGeneratorParameters(args);
+			assertEquals("test", params.getQfsmFile());
+		} catch (QfsmException e) {
+			fail("Should not be executed: " + e);
+		}
+	}
+
+	@Test
+	public void testGetQfsmFileLongPreprocess() {
+		String[] args = { "-c", "unused", "--qfsm-file=test" };
+
+		try {
+			CodeGeneratorParameters params = new CodeGeneratorParameters(args);
+			assertEquals("test", params.getQfsmFile());
+		} catch (QfsmException e) {
+			fail("Should not be executed: " + e);
+		}
+	}
+
+	@Test
+	public void testGetQfsmFileShortBroken() {
+		String[] args = { "-c", "unused", "-m" };
+
+		try {
+			new CodeGeneratorParameters(args);
+			fail("Should not be executed");
+		} catch (QfsmException e) {
+			assertEquals("Missing second argument of -m | --qfsm-file",
+					e.getMessage());
+		}
+	}
+
+	@Test
+	public void testGetQfsmFileLongBroken() {
+		String[] args = { "-c", "unused", "--qfsm-file" };
+
+		try {
+			new CodeGeneratorParameters(args);
+			fail("Should not be executed");
+		} catch (QfsmException e) {
+			assertEquals("Missing second argument of -m | --qfsm-file",
+					e.getMessage());
+		}
+	}
+
+	@Test
+	public void testGetQfsmFileUndefined() {
+		String[] args = { "-c", "unused" };
+
+		try {
+			new CodeGeneratorParameters(args);
+			fail("Should not be executed");
+		} catch (QfsmException e) {
+			assertEquals("Missing mandatory parameter: -m | --qfsm-file",
+					e.getMessage());
+		}
+	}
+
+	@Test
+	public void testUnexpectedParameter() {
+		String[] args = { "-c", "unused", "-m", "unused", "unexpected 1",
+				"-u 2", "--unexpected 3" };
+
+		try {
+			new CodeGeneratorParameters(args);
+			fail("Should not be executed");
+		} catch (QfsmException e) {
+			assertEquals(
+					"Unexpected parameters: [unexpected 1, -u 2, --unexpected 3]",
+					e.getMessage());
+		}
+	}
+}
